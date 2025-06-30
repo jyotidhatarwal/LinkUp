e4db9957ba13dccca5ce058dc89696b57b26c195
JYOTI DHATARWAL <jyoti.dhatarwal1999@gmail.com>
2025-06-30 20:11:11 +0530

    adding the http calls

--- src/app.js
+++ src/app.js
@@ -2,12 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use("/User",(req,res)=>{
-    res.send("Hello User");
-})
-
 app.use("/TestUser",(req,res)=>{
     res.send("This is Testing User");
 })
 
-app.listen(3000);
+app.get("/User",(req,res)=>{
+    console.log(req.query);
+    res.send({firstName: "Jyoti", lastName: "Dhatarwal"})
+});
+
+app.post("/User",(req,res)=>{
+    res.send("Data Successfully Added to the Database")
+});
+
+app.delete("/User",(req,res)=>{
+    res.send("Data Successfully delted from the database")
+});
+
+app.listen(3000,()=>{
+    console.log("Server is Successfully listning to port 3000.....")
+});
