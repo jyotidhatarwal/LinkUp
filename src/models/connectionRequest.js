c83f442a6f0593e5e220e9e60e622cd72c336d98
JYOTI DHATARWAL <jyoti.dhatarwal1999@gmail.com>
2025-07-14 22:15:51 +0530

    adding index for connection request db

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -20,6 +20,8 @@ const connectionRequestSchema = new mongoose.Schema({
   },
 }, {timestamps: true});
 
+connectionRequestSchema.index({fromUserId: 1, toUserId: 1});
+
 
 const connectionRequestModel = mongoose.model("ConnectionRequest", connectionRequestSchema);
 
